d8854804b54ed38dd3e421b19e4b0558944e9ac5
cagriyalniz <cagriyalniz@gmail.com>
2023-01-29 18:17:44 +0300

    fixed

--- containers/vector.hpp
+++ containers/vector.hpp
@@ -42,74 +42,106 @@ namespace ft
             ~vector(){};
             vector(const vector& other):_size(0), _capacity(0), _allocator(other._allocator), _array(NULL){ *this = other;};
             
-            //iterators
+
+			//------------Element Access------------------------//
+			//at
+			//operator[]
+			//front
+			//back
+			//data
+
+			referance at(size_type pos)
+			{
+				this->checkIndex(pos);
+				return (this->_array[pos]);
+			}
+
+			referance operator[](size_type pos)
+			{
+				this->checkIndex(pos);
+				return (this->_array[pos]);
+			}
+
+/* 			referance operator[](size_type pos) const
+			{
+				this->checkIndex(pos);
+				return (this->_array[pos]);
+			} */
+
+			referance front(void)
+			{
+				return (this->_array[0]);
+			}
+
+			referance back(void)
+			{
+				return (this->_array[this->_size - 1]);
+			}
+
+
+            //------------Iterator Functions------------------------//
+			//begin
+			//end
+			//rbegin
+			//rend
+
             iterator begin(){return iterator(this->_array);}
             iterator end(){return iterator(this->_array + this->_size);}
             iterator rbegin(){return reverse_iterator(this->_array);}
             iterator rend(){return reverse_iterator(this->_array[_size]);}
-            const_iterator cbegin(){return const_iterator(this->_array);}
-            const_reverse_iterator crend(){return const_iterator(this->_array[_size]);}
+            //const_iterator cbegin(){return const_iterator(this->_array);}
+            //const_reverse_iterator crend(){return const_iterator(this->_array[_size]);}
+		//-------------------------------------------------------//
 
-            //capacity
-            size_t size() const {return _size;}
+
+			//------------Capacity Functions------------------------//
+			//empty
+			//size
+			//max_size
+			//reserve
+			//capacity
+			//shrink_to_fit c++11
+
+			bool empty()
+			{
+				if (this->_size)
+					return (false);
+				return (true);
+			}
+
+			
+            size_t size() const {return this->_size;}
+
             size_t max_size() const {return this->_allocator.max_size();}
-            size_t capacity() const {return _capacity;}
 
-            //data
-
-            void	_reAlloc(size_type newCapacity)
+			void reserve(size_t size)
 			{
-				value_type	*newBlock;
-                //std::cout<<"_reAlloc _capacity : "<<this->_capacity<<"new capacity: "<<newCapacity<<std::endl;
-
-				if (newCapacity < this->_capacity)
-					return ;
-				newBlock = this->_allocator.allocate(newCapacity);
-				for (size_type i = 0; i < this->_size; i++)
-					this->_allocator.construct(&newBlock[i], this->_array[i]);
-				this->_allocator.deallocate(this->_array, this->_capacity);
-				this->_array = newBlock;
-				this->_capacity = newCapacity;
+				this->_reAlloc(size);
 			}
 
-			void	_smart_reAlloc(size_type newCapacity)
-			{
-				if (newCapacity <= this->_capacity)
-					return ;
-				if (this->_size == 0)
-                {
-					this->_reAlloc(1);
-                }
-				else if(newCapacity > (this->_size * 2))
-					this->_reAlloc(newCapacity);
-				else
-					this->_reAlloc(this->_size * 2);
-			}
+            size_t capacity() const {return this->_capacity;}
+		//-------------------------------------------------------//
+            
 
-            void _open_space(size_type n, size_type index_to_insert)
-			{
-				this->_smart_reAlloc(this->_size + n);
-				for (size_type i = this->_size + n - 1; i > (index_to_insert + n - 1); i--)
-					this->_allocator.construct(&this->_array[i], this->_array[i - n]);
-			}
 
+			//------------Modifiers Functions------------------------//
+			//clear
+			//insert
+			//emplace c++11
+			//erase
+			//push_back
+			//emplace_bacj c++11
+			//pop_back
+			//resize
+			//swap
+			
             void clear()
             {
                 this->_allocator.destroy(this->_array);
                 this->_size = 0;
             }
 
-
-            size_type find_new_capacity(size_type n)
-            {
-                if (this->_capacity == 0)
-                    return (n);
-                if (this->_capacity < n + this->_size)
-                    return (this->_capacity*2);
-                return this->_capacity;
-            }
-
-            iterator insert(iterator pos, const value_type& val)
+			iterator insert(iterator pos, const value_type& val)
             {
                 std::cout<<"\ninsert 2 parameter"<<std::endl;
                 
@@ -118,9 +150,7 @@ namespace ft
                 this->_allocator.construct(&this->_array[index], val);
 				this->_size++;
 				return (this->begin() + index);
-
             }
-
 
             void insert(iterator pos, size_type n,const value_type& val)
             {
@@ -133,24 +163,12 @@ namespace ft
                 this->_size += n;    
 
             }
- 
+
             //emplace c++11
 
-
-            size_type find_size_by_two_position(iterator pos_start, iterator pos_end)
+			iterator erase(iterator pos)
             {
-                size_type newSize = 0;
-                while(pos_start != pos_end)
-                {
-                    newSize++;
-                    pos_start++;
-                }
-                return this->_size - newSize;
-            }
-
-            iterator erase(iterator pos)
-            {
-                std::cout<<"\nerase func 1 arg"<<std::endl;
+                //std::cout<<"\nerase func 1 arg"<<std::endl;
   				iterator iter = pos;
 				
 				this->_allocator.destroy(&(*pos));
@@ -162,7 +180,7 @@ namespace ft
 
             iterator erase(iterator pos_start, iterator pos_end)
             {
-                std::cout<<"\nerase func 2 arg"<<std::endl;
+                //std::cout<<"\nerase func 2 arg"<<std::endl;
                 int count = 0;
                 size_type newCapacity = this->_capacity;
   				iterator iter = pos_start;
@@ -183,16 +201,13 @@ namespace ft
 
 		    void push_back(value_type value)
             {
-                //std::cout<<"push_back size : "<<this->_size<<"this->_capacity : "<< this->_capacity<<std::endl;
                 this->_smart_reAlloc(this->_size + 1);
-                //std::cout<<"this->_array[this->_size] : "<<this->_array[this->_size]<<std::endl;
                 this->_allocator.construct(&this->_array[this->_size], value);
                 this->_size++;
 
             }
 
 			//emplace_back
-
 
             void pop_back(void)
             {
@@ -201,8 +216,7 @@ namespace ft
                 this->_allocator.destroy(this->_array + this->_size - 1);
                 this->_size--;
             }
-            
-			//resize
+
 			void resize(size_type n, value_type value = value_type())
 			{
 				if (n == this->_size)
@@ -245,15 +259,138 @@ namespace ft
 
 				this->_size = tempSize;
 				this->_capacity = tempCapacity;
-				this->_allocator = other._allocator;
-				this->_array = other._array; 
+				this->_allocator = tempAlloc;
+				this->_array = tempArray; 
 			}
+		//-------------------------------------------------------//
+			
+			
+			//helping funcs
+			size_type find_new_capacity(size_type n)
+            {
+                if (this->_capacity == 0)
+                    return (n);
+                if (this->_capacity < n + this->_size)
+                    return (this->_capacity*2);
+                return this->_capacity;
+            }
 			
 
+            size_type find_size_by_two_position(iterator pos_start, iterator pos_end)
+            {
+                size_type newSize = 0;
+                while(pos_start != pos_end)
+                {
+                    newSize++;
+                    pos_start++;
+                }
+                return this->_size - newSize;
+            }
+
+			//memory functions
+            void	_reAlloc(size_type newCapacity)
+			{
+				value_type	*newBlock;
+                //std::cout<<"_reAlloc _capacity : "<<this->_capacity<<"new capacity: "<<newCapacity<<std::endl;
+
+				if (newCapacity < this->_capacity)
+					return ;
+				newBlock = this->_allocator.allocate(newCapacity);
+				for (size_type i = 0; i < this->_size; i++)
+					this->_allocator.construct(&newBlock[i], this->_array[i]);
+				this->_allocator.deallocate(this->_array, this->_capacity);
+				this->_array = newBlock;
+				this->_capacity = newCapacity;
+			}
+
+			void	_smart_reAlloc(size_type newCapacity)
+			{
+				if (newCapacity <= this->_capacity)
+					return ;
+				if (this->_size == 0)
+                {
+					this->_reAlloc(1);
+                }
+				else if(newCapacity > (this->_size * 2))
+					this->_reAlloc(newCapacity);
+				else
+					this->_reAlloc(this->_size * 2);
+			}
+
+            void _open_space(size_type n, size_type index_to_insert)
+			{
+				this->_smart_reAlloc(this->_size + n);
+				for (size_type i = this->_size + n - 1; i > (index_to_insert + n - 1); i--)
+					this->_allocator.construct(&this->_array[i], this->_array[i - n]);
+			}
 
 
+
+			void checkIndex(size_type pos)
+			{
+				if (pos >= this->_size)
+					throw OutOfBoundsException(pos);
+			}
+
+			class OutOfBoundsException : public std::exception
+			{
+				size_t _pos;
+
+				public:
+					OutOfBoundsException(int pos): _pos(pos){}
+					virtual const char* what() const throw()
+					{
+						return ("out of index\n");
+					}
+ 			};
 
     };
+
+	template< class T, class Alloc >
+	void swap( ft::vector<T,Alloc>& lhs, ft::vector<T,Alloc>& rhs )
+	{
+		lhs.swap(rhs);
+	}
+
+	template <class T, class Alloc>
+	bool operator == (const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs)
+	{
+		if (lhs.size() != rhs.size())
+			return (false);
+		return (ft::equal(lhs.begin(), lhs.end(), rhs.begin()));
+	}
+
+	template <class T, class Alloc>
+	bool operator != (const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs)
+	{
+		return (!(lhs == rhs));
+	}
+
+	template <class T, class Alloc>
+	bool operator < (const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs)
+	{
+		return (ft::lexicographical_compare(lhs.begin(), lhs.end(), rhs.begin(), rhs.end()));
+	}
+
+	template <class T, class Alloc>
+	bool operator <= (const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs)
+	{
+		return ((lhs < rhs) || (lhs == rhs));
+	}
+
+	template <class T, class Alloc>
+	bool operator > (const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs)
+	{
+		if (lhs == rhs)
+			return (false);
+		return (!(lhs < rhs));
+	}
+
+	template <class T, class Alloc>
+	bool operator >= (const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs)
+	{
+		return ((lhs > rhs) || (lhs == rhs));
+	}
 
 
 }
--- main.cpp
+++ main.cpp
@@ -1,157 +1,87 @@
 #include "./containers/vector.hpp"
-
-/* void print_vector(ft::vector<int> v)
+#include <vector>
+void print_vector(ft::vector<int> v)
 {
     ft::random_access_iterator<int>::iterator it = v.begin();
-    std::cout<<"print vector";
+    std::cout<<"print MY vector";
     std::cout<< "\n-------------\n";
-    std::cout<<"\ncapacity: "<<v.capacity()<<" size: "<<v.size()<<std::endl;
+    std::cout<<"capacity: "<<v.capacity()<<" size: "<<v.size()<<std::endl;
+    
+	std::cout<<"datas ---> ";
+    while(it != v.end())
+        std::cout<<*it++<<" ";
+    std::cout<< "\n-------------\n";
+}
+
+void print_vector(std::vector<int> v)
+{
+    std::vector<int>::iterator it = v.begin();
+    std::cout<<"print REAL vector";
+    std::cout<< "\n-------------\n";
+    std::cout<<"capacity: "<<v.capacity()<<" size: "<<v.size()<<std::endl;
     
     while(it != v.end())
         std::cout<<*it++<<" ";
     std::cout<< "\n-------------\n";
-} */
+}
 
 int main()
 {
-    ft::random_access_iterator<int>::iterator it2;
-    ft::vector<int> v1;
 
-    v1.push_back(2);
-    v1.push_back(3);
-    v1.push_back(4);
-    ft::random_access_iterator<int>::iterator it;
-    it = v1.begin();
-    std::cout<<*it<<std::endl;
-    for(;it != v1.end(); it++)
-        std::cout<<*it<<" ";
+	//vector
+	//outofindex control edilecek
 
-   // v1.clear();
-    std::cout<<"new iter"<<std::endl;
-    it2 = v1.begin();
-    for(;it2 != v1.end(); it2++)
-        std::cout<<*it2<<" ";
-    
-    ft::vector<int> v2;
-    std::cout<<"\ncapacity: "<<v2.capacity()<<" size: "<<v2.size()<<std::endl;
-    v2.insert(v2.begin(), 3, 1);
-    std::cout<<"\ncapacity: "<<v2.capacity()<<" size: "<<v2.size()<<std::endl;
-        it2 = v2.begin();
-    for(;it2 != v2.end(); it2++)
-        std::cout<<*it2<<" ";
-    std::cout<<"\n------------------------"<<std::endl;
+	/* 
+    ft::vector<int> myV1;
+    ft::random_access_iterator<int>::iterator myIt;
+	
+	print_vector(myV1);
 
-    v2.insert(v2.begin(), 3, 2);
-    std::cout<<"\ncapacity: "<<v2.capacity()<<" size: "<<v2.size()<<std::endl;
-            it2 = v2.begin();
-    for(;it2 != v2.end(); it2++)
-        std::cout<<*it2<<" ";
-    std::cout<<"\n------------------------"<<std::endl;
+	myV1.insert(myV1.begin(), 0);
+	print_vector(myV1);
 
-    v2.insert(v2.begin(), 3, 3);
-    std::cout<<"\ncapacity: "<<v2.capacity()<<" size: "<<v2.size()<<std::endl;
-            it2 = v2.begin();
-    for(;it2 != v2.end(); it2++)
-        std::cout<<*it2<<" ";
-    std::cout<<"\n------------------------"<<std::endl;
+	myV1.insert(myV1.begin(), 2, 1);
+	print_vector(myV1);
 
-    v2.insert(v2.begin(), 3, 4);
-    std::cout<<"\ncapacity: "<<v2.capacity()<<" size: "<<v2.size()<<std::endl;
-        it2 = v2.begin();
-    for(;it2 != v2.end(); it2++)
-        std::cout<<*it2<<" ";
-    std::cout<<"\n------------------------"<<std::endl;
-    
-    v2.insert(v2.begin(), 3, 5);
-    std::cout<<"\ncapacity: "<<v2.capacity()<<" size: "<<v2.size()<<std::endl;
+	myV1.erase(myV1.begin());
+	print_vector(myV1);
+
+	myV1.erase(myV1.begin(), myV1.end());
+	print_vector(myV1);
+
+	for (int i = 0; i < 10; i++)
+		myV1.push_back(i);
+	print_vector(myV1);
+	
+	
+	for (int i = 0; i < 10; i++)
+		myV1.pop_back();
+	print_vector(myV1);
+
+	myV1.resize(1);
+	print_vector(myV1);
+
+	myV1.resize(5, 42);
+	print_vector(myV1);
+	
+	myV1.resize(2);
+	print_vector(myV1);
+
+	ft::vector<int>myV2;
+	myV2.push_back(41);
+	print_vector(myV2);
+
+	myV1.swap(myV2);
+	print_vector(myV1);
 
 
-    swap(v1, v2);
-    it2 = v2.begin();
-    std::cout<<"\n----------after swap--------------"<<std::endl;
-
-    for(;it2 != v2.end(); it2++)
-        std::cout<<*it2<<" ";
-    std::cout<<"\n--------------end swap----------"<<std::endl;
-
-   
-
-/*     v2.erase(v2.begin());
-    std::cout<<"\ncapacity: "<<v2.capacity()<<" size: "<<v2.size()<<std::endl; */
-/*     v2.erase(v2.begin());
-    std::cout<<"\ncapacity: "<<v2.capacity()<<" size: "<<v2.size()<<std::endl;
-    v2.erase(v2.begin());
-    std::cout<<"\ncapacity: "<<v2.capacity()<<" size: "<<v2.size()<<std::endl; */
-
-    
-   /*  v2.clear();
-    std::cout<<"\ncapacity: "<<v2.capacity()<<" size: "<<v2.size()<<std::endl;
- */
+	myIt = myV2.begin();
+	std::cout<<*myIt<<std::endl;
+	myIt++;
+	std::cout<<*myIt<<std::endl; */
 
 
-/*     v2.push_back(22);
-    v2.push_back(32);
-    v2.push_back(42);
-    v2.push_back(52);
-    v2.push_back(62); */
-    /* v2.erase(v2.begin()); */
-   /*  ft::random_access_iterator<int>::iterator it3;
 
-        it3 = v2.begin();
-    for(;it3 != v2.end(); it3++)
-        std::cout<<*it3<<" ";
-    std::cout<<"\ncapacity: "<<v2.capacity()<<" size: "<<v2.size()<<std::endl;
-    
-    //v2.erase(v2.begin(), v2.end());
-
-    v2.resize(20);
-    std::cout<<"\ncapacity: "<<v2.capacity()<<" size: "<<v2.size()<<std::endl;
-
-    v2.resize(30, 9);
-
-    ft::random_access_iterator<int>::iterator it4;
-
-    it4 = v2.begin();
-    for(;it4 != v2.end(); it4++)
-        std::cout<<*it4<<" ";
-    std::cout<<"\ncapacity: "<<v2.capacity()<<" size: "<<v2.size()<<std::endl;
-     */
-    
-    //v2.erase(v2.begin());
-/*     v2.insert(v2.begin(), 72);
-    it2 = v2.begin();
-    for(;it2 != v2.end(); it2++)
-        std::cout<<*it2<<" "; */
-
-/*     std::cout<<"\ncapacity: "<<v2.capacity()<<" size: "<<v2.size()<<std::endl;
-    v2.insert(v2.begin(), 5);
-    std::cout<<"capacity: "<<v2.capacity()<<" size: "<<v2.size()<<std::endl;
-        v2.insert(v2.begin(), 5);
-    std::cout<<"capacity: "<<v2.capacity()<<" size: "<<v2.size()<<std::endl;
-        v2.insert(v2.begin(), 5);
-    std::cout<<"capacity: "<<v2.capacity()<<" size: "<<v2.size()<<std::endl;
-
-    ft::random_access_iterator<int>::iterator it3;
-
-    it3 = v2.begin();
-    
-    for(;it3 != v2.end(); it3++)
-        std::cout<<*it3<<" "; */
-/*     ft::random_access_iterator<int>::iterator it3;
-    it3 = v2.begin(); */
-/*     it3++;
-    it3++;
-    v2.erase(v2.begin(), it3);
-    it2 = v2.begin();
-    for(;it2 != v2.end(); it2++)
-        std::cout<<*it2<<" "; */
-/*     it3 = v2.begin();
-    it3++;
-    v2.insert(it3, 72);
-    ft::random_access_iterator<int>::iterator it4;
-    it4 = v2.begin();
-    for(;it4 != v2.end(); it4++)
-        std::cout<<*it4<<" ";  */
     
 
 }
